7d7d40ba0d9dda6ec0dcfc2b39a099951e1aa091
Afdfghjhgcfghvh <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-03-25 14:44:49 +0700

    f

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
-const dino = document.getElementById('dino');
-const rock = document.getElementById('rock');
-const score = document.getElementById('score');
+let nhacNen = new Audio("sakura.mp3")
+nhacNen.loop=true
+let nhacNhay = new Audio("onichang.mp3")
+let nhacvacham = new Audio("quec.mp3")
 
-function jump (){
-    dino.classList.add("jump-animation");
-    setTimeout(() =>{
-        dino.classList.remove('jump-animation');
-        // jump-animation: nhảy hoạt cảnh
-    }, 2000);
+function start() {
+    const dino = document.getElementById('dino');
+    const rock = document.getElementById('rock');
+    const score = document.getElementById('score');
+
+    function jump (){
+        dino.classList.add("jump-animation");
+        setTimeout(() =>{
+            dino.classList.remove('jump-animation');
+            // jump-animation: nhảy hoạt cảnh
+        }, 2000);
+    }
+
+
+    // sự kiện liên kết phím ấn thả
+    window.addEventListener('keypress', () =>{
+        if(!dino.classList.contains('jump-animation')){
+            nhacNhay.play()
+            jump();
+        }
+    });
+
+    setInterval(()=> {
+        score.innerText++;
+        // ghi bàn trong..++.
+        const dinoTop = parseInt(window.getComputedStyle(dino)
+            .getPropertyValue('top'));
+        const rockLeft = parseInt(window.getComputedStyle(rock)
+            .getPropertyValue('left'));
+        if (rockLeft < 0) {
+            rock.style.display = 'none';
+        } else{
+            rock.style.display ='';
+        }
+        // toạ độ khi bị va chạm giữa ngựa và vật
+        if (rockLeft < 140 && rockLeft > 100 && dinoTop >500){
+            nhacvacham.play()
+            alert("you got a score of:" + score.innerText +
+                "\n\nPlay again?");
+            location.reload();
+        }
+
+    }, 50);
+
 }
-
-
- // sự kiện liên kết phím ấn thả
-window.addEventListener('keypress', () =>{
-    if(!dino.classList.contains('jump-animation')){
-        jump();
-    }
-});
-
-setInterval(()=> {
-    score.innerText++;
-     // ghi bàn trong..++.
-    const dinoTop = parseInt(window.getComputedStyle(dino)
-        .getPropertyValue('top'));
-    const rockLeft = parseInt(window.getComputedStyle(rock)
-        .getPropertyValue('left'));
-    if (rockLeft < 0) {
-        rock.style.display = 'none';
-    } else{
-        rock.style.display ='';
-    }
- // toạ độ khi bị va chạm giữa ngựa và vật
-    if (rockLeft < 140 && rockLeft > 100 && dinoTop >200){
-        alert("you got a score of:" + score.innerText +
-            "\n\nPlay again?");
-        location.reload();
-    }
-
-}, 50);
